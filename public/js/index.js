b6e36a987240abb6558548d1ed6193fcb3669720
averyhw <averyhwilliams@yahoo.com>
2018-03-11 16:04:08 -0400

    Hover headings not working.

--- public/js/index.js
+++ public/js/index.js
@@ -7,35 +7,36 @@ $(document).ready(function(){
   var clickMe4 = $("#click-event4");
   var clickMe5 = $("#click-event5");
   var clickMe6 = $("#click-event6");
-  // var dropdown = $(".dropdown-pictures-nav");
 
   $(clickMe2).hide();
   $(clickMe3).hide();
   $(clickMe4).hide();
   $(clickMe5).hide();
   $(clickMe6).hide();
-  $(dropdown).hide();
 
 
 
   $("#click-me2").hover(function () {
     $("#click-event2").fadeToggle();
+    $(".header-water").toggle();
   });
   $("#click-me3").hover(function () {
     $("#click-event3").fadeToggle();
+    $(".header-pollution").toggle();
   });
   $("#click-me4").hover(function () {
     $("#click-event4").fadeToggle();
+    $(".header-health").toggle();
   });
   $("#click-me5").hover(function () {
     $("#click-event5").fadeToggle();
+    $(".header-power").toggle();
   });
   $("#click-me6").hover(function () {
     $("#click-event6").fadeToggle();
+    $(".header-biodiversity").toggle();
   });
-  // $(".expanded-menu").hover(function () {
-  //   $(dropdown).toggle();
-  // });
+
 
 
 
