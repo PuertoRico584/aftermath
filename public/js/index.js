6e54c9e997d82e06eda03c5701ec51ef23ac9da9
peytonchance <peytonchance@gmail.com>
2018-03-13 15:39:07 -0400

    Removed sassy console message from index.js

--- public/js/index.js
+++ public/js/index.js
@@ -48,9 +48,7 @@ function fadeIcons(x){
      if (i == x){
 
      }else{
-       console.log(tempId);
        $(tempId).css("opacity", "0.4");
-       console.log('yassssss bitch');
      }
    }
 }
