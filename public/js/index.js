12bf952ac8092303af8100a4d24f7ff9890f9d59
averyhw <averyhwilliams@yahoo.com>
2018-03-15 00:48:04 -0400

    Step links working.

--- public/js/index.js
+++ public/js/index.js
@@ -7,16 +7,22 @@ $(document).ready(function(){
   var clickMe4 = $("#click-event4");
   var clickMe5 = $("#click-event5");
   var clickMe6 = $("#click-event6");
+  var stepLink = $("#step-link-event");
+  var stepLink2 = $("#step-link-event2");
   var didScroll;
   var lastScrollTop = 0;
   var delta = 5;
   var navbarHeight = $('header').outerHeight();
+  var figure = $(".video-step-link").hover( hoverVideo, hideVideo );
+
 
   $(clickMe2).hide();
   $(clickMe3).hide();
   $(clickMe4).hide();
   $(clickMe5).hide();
   $(clickMe6).hide();
+  $(stepLink).hide();
+  $(stepLink2).hide();
 
 
 
@@ -35,6 +41,22 @@ $(document).ready(function(){
   $("#click-me6").hover(function () {
     $("#click-event6").fadeToggle();
   });
+
+
+
+  function hoverVideo(e) {
+      $('video', this).get(0).play();
+  }
+
+  function hideVideo(e) {
+      $('video', this).get(0).pause();
+  }
+  // $("#click-me-step-link").hover(function () {
+  //   $("#step-link-event").fadeToggle()
+  // });
+  // $("#click-me-step-link2").hover(function () {
+  //   $("#step-link-event2").fadeToggle();
+  // });
 
 
    $(window).scroll(function(event){
@@ -69,6 +91,18 @@ $(document).ready(function(){
 
        lastScrollTop = st;
    }
+   // function changeBGVideo(){
+   //   $("#click-me-step-link").css("background-color": "blue");
+   // });
+   // function changeBack(){
+   //   $("#click-me-step-link").css("background-color": "lightblue");
+   // });
+   // function changeBGVideo2(){
+   //   $("#click-me-step-link").css("background-color": "blue");
+   // });
+   // function changeBack2(){
+   //   $("#click-me-step-link").css("background-color": "lightblue");
+   // });
 
 
 
