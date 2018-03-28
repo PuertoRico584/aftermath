8653e230f49912a2ae44eaa02ca07b45610c8966
averyhw <averyhwilliams@yahoo.com>
2018-03-28 00:05:02 -0400

    Videos added.

--- public/js/about.js
+++ public/js/about.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-  $('.team-image-box').click(function(){
-    if (this.classList.contains('visible-bio')) {
-      this.classList.remove("visible-bio");
-    } else {
-      this.classList.add("visible-bio");
-    }
-  })
+  // $('.team-image-box').click(function(){
+  //   if (this.classList.contains('visible-bio')) {
+  //     this.classList.remove("visible-bio");
+  //   } else {
+  //     this.classList.add("visible-bio");
+  //   }
+  // })
+
+  // var figure = $(".about-video").hover( hoverVideo, hideVideo );
+  // function hoverVideo(e) {
+  //     $('video', this).get(0).play();
+  // }
+  // function hideVideo(e) {
+  //     $('video', this).get(0).pause();
+  //     $('video', this).get(0).currentTime = 0;
+  //     // Current time function idea from https://stackoverflow.com/questions/10457750/reset-a-video-on-mouse-out-using-html5-and-javascript
+  // }
 });
