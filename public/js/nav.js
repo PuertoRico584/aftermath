ec6189c352e0677a6d27246da2d9616a21f626c8
peytonchance <peytonchance@gmail.com>
2018-04-03 16:55:49 -0400

    Mobile navigation styles begun.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,1 +1,8 @@
-
+function toggleTitle(){
+  $('.homepage-logo').fadeToggle();
+  if ( $('.navbar-brand').css('visibility') == 'hidden' )
+    $('.navbar-brand').css('visibility', 'visible');
+  else
+    $('.navbar-brand').css('visibility','hidden');
+  $('.es-and-about').toggle();
+}
