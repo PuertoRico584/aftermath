7f11a0802634f78667b1571b2c5a271470b34bc7
peytonchance <peytonchance@gmail.com>
2018-04-03 22:51:05 -0400

    Tried to fix the positioning of the hamburger nav.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,8 +1,8 @@
-function toggleTitle(){
-  $('.homepage-logo').fadeToggle();
-  if ( $('.navbar-brand').css('visibility') == 'hidden' )
-    $('.navbar-brand').css('visibility', 'visible');
-  else
-    $('.navbar-brand').css('visibility','hidden');
-  $('.es-and-about').toggle();
-}
+// function toggleTitle(){
+//   $('.homepage-logo').fadeToggle();
+//   if ( $('.navbar-brand').css('visibility') == 'hidden' )
+//     $('.navbar-brand').css('visibility', 'visible');
+//   else
+//     $('.navbar-brand').css('visibility','hidden');
+//   $('.es-and-about').toggle();
+// }
