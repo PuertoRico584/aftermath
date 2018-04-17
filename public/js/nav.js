71cc1a6fd55010260357b51d2001f2000ccb8343
Tamara <tamara@sliceinteractive.com>
2018-04-17 09:03:50 -0400

    updated hamburger menu

--- public/js/nav.js
+++ public/js/nav.js
@@ -18,7 +18,6 @@ function toggleTitle(){
 
 function toggleTitleInner(){
   $('.navbar-brand').css('z-index','2');
-    $('.navbar-brand').removeClass('mixitup-control-active');
 
   if ( $('.es-and-about').css('display') == 'none' ){
     $('.es-and-about').css('display', 'flex');
